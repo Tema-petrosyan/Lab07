ca285cb905dc8068273f1d0b30b2cd9dfb4778b4
Artem <tema.petrosyan@gmail.com>
2021-12-19 18:52:44 +0300

    переработка кода (работает так же)

--- main.cpp
+++ main.cpp
@@ -5,34 +5,27 @@
 #include <fstream>
 #include <sstream>
 
-using std::cout;
-using std::cin;
-using std::string;
-using std::vector;
-using std::ifstream;
+using namespace std;
 
-struct furnitureItem {
+struct furnitureItem
+{
     string id;
 
     string name;
     string color;
 
-    double width;
-    double length;
-    double height;
+    double width = 0;
+    double length = 0;
+    double height = 0;
 
-    double volume;
+    double volume = 0;
 
     bool isCorrect = true;
 };
 
-void consoleSolution();
-
-void fileSolution();
-
-double str_to_double(const std::string& str)
+double str_to_double(const string& str)
 {
-    std::stringstream ss;
+    stringstream ss;
     ss << str;
 
     double result;
@@ -40,8 +33,45 @@ double str_to_double(const std::string& str)
 
     return result;
 }
+furnitureItem tempStrToItem(string* str)
+{
+    furnitureItem result;
 
-int main() {
+    str[0].erase(str[0].find(')'), 1);
+    if(stoi(str[0]) < 0)
+        result.isCorrect = false;
+    result.id = str[0];
+
+    result.name = str[1];
+
+    str[4].erase(str[4].find(';'), 1);
+    result.color = str[4];
+
+    double tempWidth = str_to_double(str[7]);
+    if(tempWidth < 0)
+        result.isCorrect = false;
+    result.width = tempWidth;
+
+    double tempLength = str_to_double(str[11]);
+    if(tempLength < 0)
+        result.isCorrect = false;
+    result.length = tempLength;
+
+    double tempHeight = str_to_double(str[15]);
+    if(tempHeight < 0)
+        result.isCorrect = false;
+    result.height = tempHeight;
+
+    result.volume = result.length * result.width * result.height;
+
+    return result;
+}
+
+vector<furnitureItem> fileInput();
+vector<furnitureItem> consoleInput();
+
+int main()
+{
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
     setlocale(LC_ALL, "");
@@ -49,11 +79,14 @@ int main() {
     size_t menu = 0;
 
     bool goodChoice = false;
-    while(!goodChoice){
+    while(!goodChoice)
+    {
         cout << "Выберите способ ввода данных:\n";
         cout << "1. Ввести вручную.\n" << "2. Чтение из файла.\n";
         cin >> menu;
-        if(!cin.good() || menu > 2 || menu < 1){
+
+        if(!cin.good() || menu > 2 || menu < 1)
+        {
             cin.ignore(225,'\n');
             cin.clear();
 
@@ -62,166 +95,108 @@ int main() {
         else goodChoice = true;
     }
 
-    switch(menu){
-        case 1: consoleSolution(); break;
-        case 2: fileSolution(); break;
+    vector<furnitureItem> itemsArray;
+
+    switch(menu)
+    {
+        case 1: itemsArray = consoleInput(); break;
+        case 2: itemsArray = fileInput(); break;
     }
+
+    size_t ruinedItemsCount = 0;
+    string maxShelfVolumeString;
+    double maxShelfVolume = 0;
+    for(furnitureItem & i : itemsArray)
+    {
+        if(i.isCorrect)
+        {
+            if(i.volume > maxShelfVolume)
+            {
+                maxShelfVolume = i.volume;
+                maxShelfVolumeString.string::append(i.id + ' ');
+            }
+        }
+        else ruinedItemsCount++;
+    }
+
+    if(!maxShelfVolumeString.empty())
+        cout << "Id шкафов с максимальным объёмом:\n" << maxShelfVolumeString << '\n';
+    else
+        cout << "В введённых данных не было ни одного шкафа, либо они введены неверно.\n";
+
+    if(ruinedItemsCount != 0)
+    {
+        char confirm;
+        cout << "Система распознала " << ruinedItemsCount << " неверно введённых предметов.\nВывести данные о них? Y/N - ";
+        cin >> confirm;
+        if(confirm == 'Y' || confirm == 'y')
+            for(furnitureItem & i : itemsArray)
+                if(!i.isCorrect)
+                {
+                    cout << "<------------------->\n";
+                    cout << "Id: " << i.id << "\n";
+                    cout << "Model: " << i.name << '\n';
+                    cout << "Color: " << i.color << '\n';
+                    cout << "Width: " << i.width << '\n';
+                    cout << "Length: " << i.length << '\n';
+                    cout << "Height: " << i.height << '\n';
+                }
+    }
+
+    return 0;
 }
 
-void fileSolution() {
-    string temp[17];
-    vector<furnitureItem> ItemsArray;
-    int itemArraySize = 0;
-    short int ruinedObjectsCount = 0;
-    double maxShelfVolume = 0;
+vector<furnitureItem> fileInput() {
+    vector<furnitureItem> result;
+    int resultSize = 0;
 
-    ifstream fin;
+    fstream stream;
     string path;
 
-    cout << "Введите путь к файлу с данными или его имя в директории с программой.\n";
+    cout << "Введите полный путь к файлу с данными.\n";
     cin >> path;
 
-    fin.open(path);
-    while(!fin.is_open()){
+    stream.open(path);
+    while (!stream.is_open()) {
         cout << "Файл не открыт. Введите путь ещё раз.\n";
-        cin.clear(); cin >> path;
+        cin.clear();
+        cin >> path;
     }
     cout << "Файл успешно открыт.\n";
 
-    while(!fin.eof()){
-        itemArraySize++;
-        ItemsArray.resize(itemArraySize);
+    while(!stream.eof()){
+        result.resize(++resultSize);
 
-        for(auto & i: temp) fin >> i;
-        temp[0].erase(temp[0].find(')'), 1);
-        if(std::stoi(temp[0]) < 0) ItemsArray.at(itemArraySize - 1).isCorrect = false;
-        ItemsArray.at(itemArraySize - 1).id = temp[0];
+        string temp[17];
+        for(string & i: temp)
+            stream >> i;
 
-        ItemsArray.at(itemArraySize - 1).name = temp[1];
-
-        temp[4].erase(temp[4].find(';'), 1);
-        ItemsArray.at(itemArraySize - 1).color = temp[4];
-
-        double tempWidth = str_to_double(temp[7]);
-        if(tempWidth < 0) ItemsArray.at(itemArraySize - 1).isCorrect = false;
-        ItemsArray.at(itemArraySize - 1).width = tempWidth;
-
-        double tempLength = str_to_double(temp[11]);
-        if(tempLength < 0) ItemsArray.at(itemArraySize - 1).isCorrect = false;
-        ItemsArray.at(itemArraySize - 1).length = tempLength;
-
-        double tempHeight = str_to_double(temp[15]);
-        if(tempHeight < 0) ItemsArray.at(itemArraySize - 1).isCorrect = false;
-        ItemsArray.at(itemArraySize - 1).height = tempHeight;
-
-        ItemsArray.at(itemArraySize - 1).volume = ItemsArray.at(itemArraySize - 1).length * ItemsArray.at(itemArraySize - 1).width * ItemsArray.at(itemArraySize - 1).height;
-        if(ItemsArray.at(itemArraySize - 1).isCorrect && ItemsArray.at(itemArraySize - 1).volume > maxShelfVolume && ItemsArray.at(itemArraySize - 1).name == "Шкаф") maxShelfVolume = ItemsArray.at(itemArraySize - 1).volume;
-        if(!ItemsArray.at(itemArraySize - 1).isCorrect) ruinedObjectsCount++;
+        result[resultSize - 1] = tempStrToItem(temp);
     }
+    stream.close();
 
-    string maxVolumeShelfString;
-    for(auto & i : ItemsArray){
-        if(i.isCorrect && i.name == "Шкаф"){
-            if(i.volume == maxShelfVolume)
-                maxVolumeShelfString.string::append(i.id + ' ');
-        }
-    }
-
-    if(maxVolumeShelfString.empty()) cout << "В введённых записях не было ни одного шкафа.\n";
-    else {
-        cout << "\nId шкафов с наибольшим объёмом: ";
-        cout << maxVolumeShelfString << '\n';
-    }
-
-    if(ruinedObjectsCount != 0){
-        char confirm;
-        cout << "Некоторые объекты(" << ruinedObjectsCount << ") были введены неверно, и не учитывались в решении. Вывести данные о них? y/n - ";
-        cin >> confirm;
-        if(confirm == 'Y' || confirm == 'y')
-            for(auto & i : ItemsArray)
-                if(!i.isCorrect){
-                    cout << "<------------------->\n";
-                    cout << "Id: " << i.id << "\n";
-                    cout << "Model: " << i.name << '\n';
-                    cout << "Color: " << i.color << '\n';
-                    cout << "Width: " << i.width << '\n';
-                    cout << "Length: " << i.length << '\n';
-                    cout << "Height: " << i.height << '\n';
-                }
-    }
+    return result;
 }
 
-void consoleSolution() {
-    string temp[17];
-    vector<furnitureItem> ItemsArray;
-    int itemArraySize;
-    short int ruinedObjectsCount = 0;
-    double maxShelfVolume = 0;
+vector<furnitureItem> consoleInput()
+{
+    vector<furnitureItem> result;
+    int resultSize = 0;
 
     cout << "Введите количество записей которые хотите обработать.\n k - ";
-    cin >> itemArraySize;
-    ItemsArray.resize(itemArraySize);
+    cin >> resultSize;
+    result.resize(resultSize);
 
     cout << "Вводите записи в формате:\n\t _id)_name_цвет_-_color;_ширина_-_width_см;_длина_-_length_см;_высота_-_height_см\n";
 
-    for(auto & i : ItemsArray){
-        for(auto & j : temp) cin >> j;
+    for(furnitureItem i : result)
+    {
+        string temp[17];
+        for(string j : temp)
+            cin >> j;
 
-        temp[0].erase(temp[0].find(')'), 1);
-        if(std::stoi(temp[0]) < 0) i.isCorrect = false;
-        i.id = temp[0];
-
-        i.name = temp[1];
-
-        temp[4].erase(temp[4].find(';'), 1);
-        i.color = temp[4];
-
-        double tempWidth = str_to_double(temp[7]);
-        if(tempWidth < 0) i.isCorrect = false;
-        i.width = tempWidth;
-
-        double tempLength = str_to_double(temp[11]);
-        if(tempLength < 0) i.isCorrect = false;
-        i.length = tempLength;
-
-        double tempHeight = str_to_double(temp[15]);
-        if(tempHeight < 0) i.isCorrect = false;
-        i.height = tempHeight;
-
-        i.volume = i.length * i.width * i.height;
-        if(i.isCorrect && i.volume > maxShelfVolume && i.name == "Шкаф") maxShelfVolume = i.volume;
-        if(!i.isCorrect) ruinedObjectsCount++;
+        result[resultSize - 1] = tempStrToItem(temp);
     }
 
-
-    string maxVolumeShelfString;
-    for(auto & i : ItemsArray){
-        if(i.isCorrect && i.name == "Шкаф"){
-            if(i.volume == maxShelfVolume)
-                maxVolumeShelfString.string::append(i.id + ' ');
-        }
-    }
-
-    if(maxVolumeShelfString.empty()) cout << "В введённых записях не было ни одного шкафа.\n";
-    else {
-        cout << "\nId шкафов с наибольшим объёмом: ";
-        cout << maxVolumeShelfString << '\n';
-    }
-
-    if(ruinedObjectsCount != 0){
-        char confirm;
-        cout << "Некоторые объекты(" << ruinedObjectsCount << ") были введены неверно, и не учитывались в решении. Вывести данные о них? y/n - ";
-        cin >> confirm;
-        if(confirm == 'Y' || confirm == 'y')
-            for(auto & i : ItemsArray)
-                if(!i.isCorrect){
-                    cout << "<------------------->\n";
-                    cout << "Id: " << i.id << "\n";
-                    cout << "Model: " << i.name << '\n';
-                    cout << "Color: " << i.color << '\n';
-                    cout << "Width: " << i.width << '\n';
-                    cout << "Length: " << i.length << '\n';
-                    cout << "Height: " << i.height << '\n';
-                }
-    }
+    return result;
 }
